2b47b3663a7b65d23705c84e4780c5512ec43dc4
reppohc123 <33378890+reppohc123@users.noreply.github.com>
2022-04-19 09:21:31 +0200

    H1 + Liist with three elements

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>Why Lufy is Joyboy</h1>
+    <ul>
+      <li>Gomu Gomu no Fruit is a Zoan Fruit</li>
+      <li>Awakining looks like NIKA</li>
+      <li>Chapter 1009</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
