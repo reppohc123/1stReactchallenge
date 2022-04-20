f0eaea0d2654ac9edbe02194ab020575f4d9961e
reppohc123 <33378890+reppohc123@users.noreply.github.com>
2022-04-20 06:53:06 +0200

    grammar mistakes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ var number = Math.floor(Math.random() * max);
 ReactDom.render(
   <div>
     {" "}
-    <h1 className="heading">Why Lufy is Joyboy</h1>{" "}
+    <h1 className="heading">Why Luffy is Joyboy?</h1>{" "}
     <ul>
       {" "}
       <li>Gomu Gomu no Fruit is a Zoan Fruit</li>{" "}
@@ -14,7 +14,7 @@ ReactDom.render(
     <p>Waht is your devil fruit?</p>{" "}
     <ul>
       {" "}
-      <li>1 Doku Dokuno Mi</li> <li>2 Hie Hie no Mi</li>{" "}
+      <li>1 Doku Doku no Mi</li> <li>2 Hie Hie no Mi</li>{" "}
       <li>3 Soru Soru no Mi</li> <li>4 Gura Gura no Mi</li>{" "}
       <li>5 Ope Ope no Mi</li> <li>6 Goro Goro no Mi</li>{" "}
       <li>7 Sara Sara no Mi</li> <li>8 Yami Yami no Mi</li>{" "}
