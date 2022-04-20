7c0bd179c10e4ed1cad377608cdd464885015884
reppohc123 <33378890+reppohc123@users.noreply.github.com>
2022-04-20 06:02:12 +0200

    random number

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from "react";
 import ReactDom from "react-dom";
-
+var max = 9;
+var number = Math.floor(Math.random() * max);
 ReactDom.render(
   <div>
-    <h1>Why Lufy is Joyboy</h1>
+    {" "}
+    <h1>Why Lufy is Joyboy</h1>{" "}
     <ul>
-      <li>Gomu Gomu no Fruit is a Zoan Fruit</li>
-      <li>Awakining looks like NIKA</li>
-      <li>Chapter 1009</li>
-    </ul>
+      {" "}
+      <li>Gomu Gomu no Fruit is a Zoan Fruit</li>{" "}
+      <li>Awakining looks like NIKA</li> <li>Chapter 1009</li>{" "}
+    </ul>{" "}
+    <p>Waht is your devil fruit?</p>{" "}
+    <ul>
+      {" "}
+      <li>1 Doku Dokuno Mi</li> <li>2 Hie Hie no Mi</li>{" "}
+      <li>3 Soru Soru no Mi</li> <li>4 Gura Gura no Mi</li>{" "}
+      <li>5 Ope Ope no Mi</li> <li>6 Goro Goro no Mi</li>{" "}
+      <li>7 Sara Sara no Mi</li> <li>8 Yami Yami no Mi</li>{" "}
+      <li>9 Gomu Gomu no Mi</li>{" "}
+    </ul>{" "}
+    <p>Your number is {number}!</p>{" "}
   </div>,
   document.getElementById("root")
 );
